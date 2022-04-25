9f557007400bf5f04585686e59c7d9355adff475
Jasmine Tan <jasmine.tan6032@gmail.com>
2022-04-25 19:05:46 +0100

    adapt for smaller files

--- Regressions_with_behaviour.m
+++ Regressions_with_behaviour.m
@@ -2,6 +2,12 @@
    %load data
     trialmatrix_filename = [filedir 'Part' num2str(part) filename];
     load(trialmatrix_filename)
+
+%remove unnecessary fields
+    trialmatrix_clean = rmfield(trialmatrix_clean, 'reconstruct_pcorrect_eeg_final');
+    trialmatrix_clean = rmfield(trialmatrix_clean, {'pcorrect_reg_regress_weights', 'reconstruct_pcorrect_reg', 'STV_reg_regress_electrodes', 'pcorrect_regress_weights', 'reconstruct_pcorrect', 'STV_regress_electrodes', 'pcorrect_regress_weights_eeg_alt'});
+trialmatrix_clean = rmfield(trialmatrix_clean, 'EEGdata');
+
         
 %remove noisy epochs
         noisy = [trialmatrix.noisy];
@@ -10,7 +16,7 @@
 
 pcorrect = cell2mat({trialmatrix_clean.majPCorrect})';
 condition = {trialmatrix_clean.type}';
-amplitude = cell2mat({trialmatrix_clean.regression_amplitude})';
+amplitude = cell2mat({trialmatrix_clean.STV_regress_eeg_final})';
 tbl = table(pcorrect,condition,amplitude,'VariableNames',{'pCorrect', 'condition','amplitude'});
 tbl.condition = categorical(tbl.condition);
 %with interaction term
@@ -21,19 +27,19 @@ lm2 = fitlm(tbl,'amplitude~pCorrect+condition');
 answer = cell2mat({trialmatrix_clean.answer})';
 tbl2 = table(pcorrect,answer,amplitude,'VariableNames',{'pCorrect', 'answer','amplitude'});
 tbl2.answer = categorical(tbl2.answer);
-lm2 = fitlm(tbl2,'amplitude~pCorrect+answer');
+lm3 = fitlm(tbl2,'amplitude~pCorrect+answer');
 
 pcorrect = cell2mat({trialmatrix_clean.majPCorrect})';
 condition = {trialmatrix_clean.type}';
 amplitude = cell2mat({trialmatrix_clean.Pz_amplitude})';
 tbl = table(pcorrect,condition,amplitude,'VariableNames',{'pCorrect', 'condition','amplitude'});
 tbl.condition = categorical(tbl.condition);
-lm3 = fitlm(tbl,'amplitude~pCorrect+condition');
+lm4 = fitlm(tbl,'amplitude~pCorrect+condition');
 
 answer = cell2mat({trialmatrix_clean.answer})';
 tbl2 = table(pcorrect,answer,amplitude,'VariableNames',{'pCorrect', 'answer','amplitude'});
 tbl2.answer = categorical(tbl2.answer);
-lm4 = fitlm(tbl2,'amplitude~pCorrect+answer');
+lm5 = fitlm(tbl2,'amplitude~pCorrect+answer');
 
 gscatter(tbl2.amplitude,tbl2.pCorrect,tbl2.answer)
 hold on
