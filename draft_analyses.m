65d955572bb437859eff0f016a51f36a5fbd7542
Jasmine Tan <jasmine.tan6032@gmail.com>
2022-04-27 11:26:49 +0100

    ideas for analyses

---
+++ draft_analyses.m
@@ -0,0 +1,26 @@
+%plot histograms of reaction times by condition
+
+%plot ERPs (Pz and weighted average EEG) for high and low pcorrect by condition
+
+%Sensitivity to information inherent in flip is modulated by confidence
+%(only confidence trials)
+
+
+%Sensitivity to information provided by flip (strength of the P300-like
+%activity) is modulated by the current available evidence
+
+%what's the individual's average pcorrect threshold for fixed and
+%decreasing win conditions?
+
+%distinguish neural activity related to accumulating evidence for correct colour and other colour
+
+%what happens in the long period where pcorrect stays the same? How does
+%amplitude vary while pcorrect stays flat? People are calibrating
+%confidence? Being extra sure? Still gathering evidence? Haven't picked up
+%that there is already a clear answer?
+
+%plot STV over flips (but how to account for different lengths of trials?)
+
+%majority and minority still having a role to play at marking the two
+%different options you are accumulating evidence for. 
+
