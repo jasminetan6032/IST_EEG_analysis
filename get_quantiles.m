e5ab2a1f85cf1ec5cff400bcd55a98583aa86597
Jasmine Tan <jasmine.tan6032@gmail.com>
2022-05-19 13:25:25 +0100

    started code for getting quantiles for plotting interactions

---
+++ get_quantiles.m
@@ -0,0 +1,24 @@
+%get_quantiles
+%get fixed quantiles
+load('C:\Github\IST_EEG_analysis\EEG\Updated\Part2_EEG_regression_weighted_STV.mat')
+    pcorrect = cell2mat({trialmatrix_clean.majPCorrect})';
+    pcorrect_change = cell2mat({trialmatrix_clean.PCorrectChange})';
+    pcorrect_previous = cell2mat({trialmatrix_clean.previousPCorrect})';
+pcorrect_unique = unique(pcorrect);
+Qu = quantile(pcorrect_unique,4);
+pcorrect_change_unique = unique(pcorrect_change);
+Qu_change = quantile(pcorrect_change_unique,4);
+pcorrect_previous_unique = unique(pcorrect_previous);
+Qu_previous = quantile(pcorrect_previous_unique,2);
+
+%get individualised bins
+    pcorrect = cell2mat({trialmatrix_clean.majPCorrect})';
+    pcorrect_change = cell2mat({trialmatrix_clean.PCorrectChange})';
+    pcorrect_previous = cell2mat({trialmatrix_clean.previousPCorrect})';
+
+Q = quantile(pcorrect,4);
+
+Q_change = quantile(pcorrect_change,4);
+
+Q_previous = quantile(pcorrect_previous,2);
+
