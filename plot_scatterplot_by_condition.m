ba3a42dc5d258a57ca52617a59cb8878aac8405a
Jasmine Tan <jasmine.tan6032@gmail.com>
2022-05-21 23:32:38 +0100

    plotting P300s and confidence by answer interaction

--- plot_scatterplot_by_condition.m
+++ plot_scatterplot_by_condition.m
@@ -1,14 +1,14 @@
-save_filedir = 'C:\Github\IST_EEG_analysis\Regression_results\Images\';
+save_filedir = 'C:\Github\IST_EEG_analysis\Regression_results\Answer\By_behaviour\';
 
 %plot interaction with scatter plots
 w = linspace(min(tbl.pCorrect),max(tbl.pCorrect));
-fit = fitlm(tbl,'interactions','ResponseVar','amplitude','PredictorVars',{'pCorrect', 'condition'},'CategoricalVars','condition');
+fit = fitlm(tbl,'interactions','ResponseVar','amplitude','PredictorVars',{'pCorrect', 'answer'},'CategoricalVars','answer');
 
 figure()
-gscatter(tbl.pCorrect,tbl.amplitude,tbl.condition,'br','xo')
-line(w,feval(fit,w,'decreasing'),'Color','b','LineWidth',2)
-line(w,feval(fit,w,'fixed'),'Color','r','LineWidth',2)
-title('Fitted Regression Lines by Condition')
-savefilename = [save_filedir 'Part12_condition_sig_interaction.jpg'];
+gscatter(tbl.pCorrect,tbl.amplitude,tbl.answer,'br','xo')
+line(w,feval(fit,w,'0'),'Color','b','LineWidth',2)
+line(w,feval(fit,w,'1'),'Color','r','LineWidth',2)
+title('Fitted Regression Lines by decisionPoint')
+savefilename = [save_filedir 'sig_participants_regression_answer_STV.jpg'];
 saveas(gca,savefilename);
 
