7b1a52e1888cdd177323a60c372c2eeafca7f244
Jasmine Tan <jasmine.tan6032@gmail.com>
2022-08-11 16:59:13 +0100

    investigating context and change in pcorrect with line plot

--- Regression_results/PCorrectChange_Previous_PCorrect/Quantiles/low_to_high_context_change_low_to_high_vs_STV.m
+++ Regression_results/PCorrectChange_Previous_PCorrect/Quantiles/low_to_high_context_change_low_to_high_vs_STV.m
@@ -7,6 +7,7 @@ for part = 1:22
     
     trialmatrix_filename = [filedir 'Part' num2str(part) filename];
     load(trialmatrix_filename);
+    %extract context and divide it into quantiles
     pcorrect_previous = cell2mat({trialmatrix_clean.previousPCorrect})';
     Q_previous = quantile(pcorrect_previous,2);
     
@@ -20,37 +21,41 @@ for part = 1:22
             trialmatrix_clean(row).PreviousBin = 3;
         end
     end
+    %extract change, STV and bin numbers for context
     pcorrect_change = cell2mat({trialmatrix_clean.PCorrectChange})';
     STV = cell2mat({trialmatrix_clean.STV_regress_eeg_final});
     previous_bins = cell2mat({trialmatrix_clean.PreviousBin});
     
+    %subset by each context bin and divide further into change quantiles
     for bin = 1:3
         bin_previous = find(previous_bins == bin);
         STV_previous_bin = STV(bin_previous);
-        pcorrect_previous_bin = pcorrect_change(bin_previous);
-        Q_change = quantile(pcorrect_previous_bin,2);
+        pcorrect_previous_bin = pcorrect_previous(bin_previous);
+        pcorrect_change_bin = pcorrect_change(bin_previous);
+        Q_change = quantile(pcorrect_change_bin,2);
         
-        for row = 1:length(pcorrect_previous_bin)
-            x = pcorrect_previous_bin(row);
+        for row = 1:length(pcorrect_change_bin)
+            x = pcorrect_change_bin(row);
             if x <= Q_change(1)
-                pcorrect_previous_bin(row,2) = 1;
+                pcorrect_change_bin(row,2) = 1;
             elseif x > Q_change(1) && x <= Q_change(2)
-                pcorrect_previous_bin(row,2)= 2;
+                pcorrect_change_bin(row,2)= 2;
             elseif x > Q_change(2)
-                pcorrect_previous_bin(row,2) = 3;
+                pcorrect_change_bin(row,2) = 3;
             end
         end
         
         for change_bin = 1:3
-            pcorrect_change_bin_index = find(pcorrect_previous_bin(:,2)==change_bin);
-            pcorrect_change_bin(bin,change_bin) = mean(STV_previous_bin(pcorrect_change_bin_index));
-            pcorrect_change_bin(bin,4) = bin;
-            average_change(bin,change_bin) = mean(pcorrect_previous_bin(pcorrect_change_bin_index));
+            pcorrect_change_bin_index = find(pcorrect_change_bin(:,2)==change_bin);
+            average_STV(bin,change_bin) = mean(STV_previous_bin(pcorrect_change_bin_index));
+            average_context(bin,change_bin) = mean(pcorrect_previous_bin(pcorrect_change_bin_index));
+            average_change(bin,change_bin) = mean(pcorrect_change_bin(pcorrect_change_bin_index));
         end 
             
         
         
-         participants(part).plotdata = pcorrect_change_bin;
+         participants(part).STVdata = reshape(average_STV',1,[]);
+         participants(part).change = average_change;
 %         participants(part).plotdata(bin,2) = mean(pcorrect_change(bin_previous));
 %         participants(part).plotdata(bin,3) = mean(STV(bin_previous));
     end
@@ -73,3 +78,24 @@ s = scatter(tbl,'change','EEG','filled','ColorVariable','context');
 
 figure; scatter(change,STV,[],context,'filled')
 colorbar
+
+
+avg_data = [];
+for part = 1:22
+    participant = participants(part).STVdata;
+    avg_data = cat(3,avg_data, participant);
+end
+
+plotdata = mean(avg_data,3);
+plotdata = plotdata(:,1:3);
+
+plotdata = plotdata';
+line1 = plotdata(:,1);
+line2 = plotdata(:,2);
+line3 = plotdata(:,3);
+x = 1:3;
+figure;plot(x,line1,x,line2,x,line3);
+
+test = reshape(plotdata',1,[])';
+context_bins = [1,1,1,2,2,2,3,3,3];
+test_cat = cat(2,test,context_bins');
